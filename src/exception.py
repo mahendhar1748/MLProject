02beae46c445fb411cccefe71b48ef4bf242b02f
mahendhar1748 <mahendhar1748@gmail.com>
2024-05-18 10:55:15 +0530

    git add . git commit -m setup git push -u origin main

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 '''contains methods and variables for modifying many elements of the Python Runtime Environment.'''
 
 def error_message_detail(error,error_detail:sys):
@@ -17,11 +17,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-'''    
+'''
 if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
         logging.info("Divided by ZERO")
         raise CustomException(e,sys)
-'''    
+ '''  
